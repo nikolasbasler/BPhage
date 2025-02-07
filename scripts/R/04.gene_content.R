5edad8a716fa34a42efea85dd3b9da738a8ef86e
nikolasbasler <nikolasbasler@posteo.de>
2025-02-07 17:36:34 +0100

    plots for sulf tpm

--- scripts/R/04.gene_content.R
+++ scripts/R/04.gene_content.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(ggpubr)
+library(multcompView)
+
+set.seed(1)
 
 metadata <- readRDS("output/R/R_variables/metadata.RDS")
 classification <- readRDS("output/R/R_variables/classification.RDS")
@@ -246,8 +249,127 @@ moron_pie <-
   theme(legend.margin=margin(0,2,0,-20)) +
   scale_fill_manual(values = moron_pie_colors)
 
+
+## Sulfur metabolism gene
+
+phage_tpm <- read.csv("output/R/relative_abundance/phage_tpm.csv")
+
+sulfur_phage_annot <- phold_predictions_with_extensions %>%
+  # filter(str_detect(product, "sulf")) %>%
+  filter(product == "phosphoadenosine phosphosulfate reductase") %>% 
+  inner_join(classification, ., by = join_by("contig" == "contig_id"))
+
+sulf_meta <- phage_tpm %>%
+  pivot_longer(-contig, names_to = "Sample_ID", values_to = "TPM") %>%
+  mutate(sulf_metabolism = ifelse(contig %in% sulfur_phage_annot$contig, TRUE, FALSE)) %>%
+  left_join(., metadata, by = "Sample_ID") %>% 
+  select(contig, Sample_ID, TPM, sulf_metabolism, Hive_ID, Country, Season, Gut_part, Health) %>%
+  filter(Gut_part == "rec") %>% # Focus on rectum for more stable values (some midgut samples have very few phages)
+  group_by(Sample_ID, sulf_metabolism) %>% 
+  mutate(sulf_tpm = sum(TPM)) %>%
+  ungroup()
+
+sulf_positive_hives <- sulf_meta %>%
+  filter(sulf_tpm > 0) %>%
+  group_by(Country) %>%
+  summarise(hive_count = n_distinct(Hive_ID),
+            sulf_positive_hives = n_distinct(Hive_ID[sulf_metabolism])) %>%
+  mutate(sulf_positive_prop = sulf_positive_hives / hive_count)
+
+meta_variables <- c("Hive_ID", "Country", "Season", "Health")
+sulf_stats <-  list()
+tpm_KW <- list()
+tpm_pwc <- list()
+sulf_tpm_plots <- list()
+genomes_KW <- list()
+genomes_pwc <- list()
+sulf_genome_prop_plots <- list()
+for (met_v in meta_variables) {
+  tpm <- sulf_meta %>%
+    filter(sulf_metabolism) %>%
+    group_by(.data[[met_v]]) %>%
+    mutate(mean_sulf_tpm = mean(sulf_tpm)) %>%
+    filter(mean_sulf_tpm > 0) %>%
+    ungroup() %>%
+    select(Sample_ID, all_of(meta_variables), sulf_tpm, mean_sulf_tpm) %>%
+    distinct() %>%
+    arrange(desc(mean_sulf_tpm))
+  tpm_KW[[met_v]] <- kruskal.test(tpm$sulf_tpm ~ tpm[[met_v]]) %>%
+    unlist() %>% as.matrix() %>% t() %>% as_tibble() %>% 
+    rename(chi_squared = `statistic.Kruskal-Wallis chi-squared`) %>%
+    mutate(parameter.df = as.numeric(parameter.df),
+           p.value = as.numeric(p.value),
+           chi_squared = as.numeric(chi_squared))
+  # See if I can make this work. Test with Season to see the problem. Letters aren't assigned to every group.
+  # pwc_p <- pairwise.wilcox.test(tpm$sulf_tpm, tpm[[met_v]], p.adjust.method = "BH")
+  # all_groups <- unique(c(rownames(pwc_p$p.value), colnames(pwc_p$p.value)))
+  # full_p_matrix <- as.data.frame(matrix(1, nrow = length(all_groups), ncol = length(all_groups), 
+  #                                       dimnames = list(all_groups, all_groups)))
+  # full_p_matrix[rownames(pwc_p$p.value), colnames(pwc_p$p.value)] <- pwc_p$p.value
+  # multcompLetters(full_p_matrix)$Letters
+  # tpm_pwc[[met_v]] <- multcompLetters(pwc_p$p.value)$Letters
+
+  genomes <- sulf_meta %>%
+    filter(TPM >0) %>%
+    group_by(Sample_ID, sulf_metabolism) %>% 
+    mutate(sulf_genomes = n()) %>%
+    group_by(Sample_ID) %>%
+    mutate(sample_sulf_genome_props = sulf_genomes / n ()) %>% 
+    group_by(.data[[met_v]]) %>%
+    mutate(sulf_genome_count = sum(sulf_metabolism),
+           sulf_genome_prop = mean(sulf_metabolism)) %>%
+    ungroup() %>%
+    filter(sulf_metabolism) %>%
+    select(Sample_ID, all_of(meta_variables), sample_sulf_genome_props, sulf_genome_prop) %>%
+    distinct() %>%
+    arrange(desc(sulf_genome_prop))
+  genomes_KW[[met_v]] <- kruskal.test(genomes$sample_sulf_genome_props ~ genomes[[met_v]]) %>%
+    unlist() %>% as.matrix() %>% t() %>% as_tibble() %>% 
+    rename(chi_squared = `statistic.Kruskal-Wallis chi-squared`) %>%
+    mutate(parameter.df = as.numeric(parameter.df),
+           p.value = as.numeric(p.value),
+           chi_squared = as.numeric(chi_squared))
+  pwc_p <- pairwise.wilcox.test(genomes$sample_sulf_genome_props, genomes[[met_v]], p.adjust.method = "BH")
+  genomes_pwc[[met_v]] <- multcompLetters(pwc_p$p.value)$Letters
+  
+  sulf_tpm_plots[[met_v]] <- tpm %>%
+    # mutate(!!met_v := factor(.data[[met_v]], levels = unique(tpm[[met_v]]))) %>%
+    ggplot(aes(x = .data[[met_v]], y = sulf_tpm)) +
+    geom_boxplot(outliers = FALSE) +
+    geom_jitter(alpha = 0.33, width = 0.25) +
+    # scale_y_continuous(trans='log10') + # This would change the picture because 0s would be ignored.
+    ggtitle(paste0("KW p-value: ", tpm_KW[[met_v]]$p.value)) # +
+    # scale_y_continuous(limits = c(0,0.1))
+
+  sulf_genome_prop_plots[[met_v]] <- genomes %>%
+    # mutate(!!met_v := factor(.data[[met_v]], levels = unique(genomes[[met_v]]))) %>%
+    ggplot(aes(x = .data[[met_v]], y = sample_sulf_genome_props)) +
+    geom_boxplot(outliers = FALSE) +
+    geom_jitter(alpha = 0.33, width = 0.25) +
+    ggtitle(paste0("KW p-value: ", genomes_KW[[met_v]]$p.value))
+  
+  if (met_v %in% c("Hive_ID", "Season")) {
+    sulf_tpm_plots[[paste0(met_v,"_facet")]] <- sulf_tpm_plots[[met_v]] +
+      facet_wrap(~Country, scales = "free_x") +
+      ggtitle("")
+    sulf_genome_prop_plots[[paste0(met_v,"_facet")]] <- sulf_genome_prop_plots[[met_v]]  +
+      facet_wrap(~Country, scales = "free_x") +
+      ggtitle("")
+  }
+  
+  summarised_tpm <- tpm %>%
+    select(all_of(met_v), mean_sulf_tpm) %>%
+    distinct()
+  summarised_genomes <- genomes %>% 
+    select(all_of(met_v), sulf_genome_prop) %>% 
+    distinct()
+  sulf_stats[[met_v]] <- left_join(summarised_tpm, summarised_genomes, by = met_v)
+}
+
+
+
 ### Save files ####
-system("mkdir -p output/R/gene_content")
+system("mkdir -p output/R/gene_content/sulfur")
 # for (plot in names(annotation_core_plots)) {
 #   wid <- 12
 #   hig <- 8
@@ -272,95 +394,20 @@ write_csv(moron_grouping, "output/R/gene_content/core_moron_pie.csv")
 write_csv(contigs_with_TA, "output/R/gene_content/core_TA_contigs.csv")
 write_csv(genes_in_core, "output/R/gene_content/core_all_products.csv")
 
+## Sulfur
 
-## Work in progress: Sulfor metabolism
-sulfur_phage_annot <- phold_predictions_with_extensions %>%
-  # filter(str_detect(product, "sulf")) %>%
-  filter(product == "phosphoadenosine phosphosulfate reductase") %>% 
-  inner_join(classification, ., by = join_by("contig" == "contig_id"))
-
-sulfur_phage_annot %>%
-  select(contig) %>%
-  distinct()
-
-sulfur_phage_annot %>%
-  select(contig) %>%
-  filter(contig %in% present_in_all_countries) %>%
-  distinct()
-
-meta_v <- c("Family", "Genus", "Core")
-
-# I'm not sure where I was getting at witht his...
-# overall_vs_sulf <- list()
-# for (met in meta_v) {
-#   overall_props <- classification %>%
-#     group_by(.data[[met]]) %>%
-#     summarise(overall_genome_count = n()) %>%
-#     arrange(desc(overall_genome_count)) %>%
-#     mutate(overall_genome_prop = overall_genome_count / sum(overall_genome_count))
-#   
-#   overall_vs_sulf[[met]] <- sulfur_phages %>%
-#     group_by(.data[[met]]) %>%
-#     summarise(sulf_genome_count = n()) %>%
-#     arrange(desc(sulf_genome_count)) %>%
-#     mutate(sulf_genome_prop = sulf_genome_count / sum(sulf_genome_count)) %>%
-#     inner_join(., overall_props) %>%
-#     ggplot(aes(x = sulf_genome_prop, y = overall_genome_prop)) +
-#     geom_point() +
-#     labs(title = met)
-# }
-
-
-phage_tpm <- read.csv("output/R/relative_abundance/phage_tpm.csv")
-
-sulfur_phage_meta <- phage_tpm %>%
-  filter(contig %in% sulfur_phage_annot$contig) %>%
-  pivot_longer(-contig, names_to = "Sample_ID", values_to = "TPM") %>%
-  filter(TPM > 0) %>%
-  left_join(., metadata, by = "Sample_ID") %>%
-  select(contig, Sample_ID, TPM, Hive_ID, Country, Season, Gut_part, Health)
-
-summed_suf_tpms <- sulfur_phage_meta %>%
-  group_by(Sample_ID) %>%
-  mutate(summed_sample_tpm = sum(TPM)) %>%
-  ungroup() %>% 
-  select(Country, summed_sample_tpm) %>% 
-  distinct()
-mean_sulf_tpms <- summed_suf_tpms %>%
-  group_by(Country) %>%
-  summarise(mean_TPM = mean(summed_sample_tpm)) %>%
-  arrange(desc(mean_TPM))
-
-meta_variables <- c("Country", "Season", "Gut_part")
-sulf_plot_tpm <- list()
-sulf_plot_phage_count <- list()
-sulf_stats <- list()
-tpm_KW_p_values <- list()
-for (meta_v in meta_variables) {
-  
-  tpm_KW_p_values[[meta_v]] <- kruskal.test(sulfur_phage_meta$TPM~sulfur_phage_meta[[meta_v]])$p.value
-
-  sullf_mean_tpm <- sulfur_phage_meta %>%
-    group_by(.data[[meta_v]]) %>%
-    summarise(mean_TPM = mean(TPM))
-  sulf_stats[[meta_v]] <- sulfur_phage_meta %>%
-    select(contig, all_of(meta_v)) %>%
-    distinct() %>%
-    group_by(.data[[meta_v]]) %>%
-    summarise(phage_count = n()) %>%
-    left_join(sullf_mean_tpm, ., by = meta_v) %>%
-    arrange(desc(mean_TPM))
-  
-  sulf_plot_tpm[[meta_v]] <- sulfur_phage_meta %>%
-    ggplot(aes(x = .data[[meta_v]], y = TPM)) +
-    geom_boxplot() +
-    scale_y_continuous(trans='log10') +
-    ggtitle(paste0("KW p-value: ", KW_p_values[[meta_v]]))
+write_delim(sulf_positive_hives, "output/R/gene_content/sulfur/sulfur_positive_hives.tsv", delim = "\t ")
+for (meta in names(sulf_stats)) {
+  write_delim(sulf_stats[[meta]], paste0("output/R/gene_content/sulfur/sulfur_stats.", meta, ".tsv", delim = "\t"))
+  write_delim(tpm_KW[[meta]], paste0("output/R/gene_content/sulfur/tpm_krusk.", meta, ".tsv", delim = "\t"))
+  write_delim(genomes_KW[[meta]], paste0("output/R/gene_content/sulfur/genome_count_krusk.", meta, ".tsv", delim = "\t"))
 }
-
-sulf_plot_tpm
-sulf_plot_phage_count
-sulf_stats
-tpm_KW_p_values
-#
+for (meta in names(sulf_tpm_plots)) {
+  ggsave(paste0("output/R/gene_content/sulfur/tpm.", sulf_tpm_plots[[meta]], ".pdf"),
+         width = 7, height = 7)
+  ggsave(paste0("output/R/gene_content/sulfur/genome_count.", sulf_genome_prop_plots[[meta]], ".pdf"),
+         width = 7, height = 7)
+  
+  as.matrix() %>% t() %>% as_tibble()
+}
 
