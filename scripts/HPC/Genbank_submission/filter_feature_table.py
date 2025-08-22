5976ffa60ba3e57317c0710bfbb268f8918ba6c0
nikolasbasler <nikolasbasler@posteo.de>
2025-08-22 18:36:12 +0200

    bounce to hpc

--- scripts/HPC/Genbank_submission/filter_feature_table.py
+++ scripts/HPC/Genbank_submission/filter_feature_table.py
@@ -10,12 +10,14 @@ wanted = {line.strip() for line in open(id_file) if line.strip()}
 with open(in_tbl, "r") as tbl, open(out_tbl, "w") as outfile:
     is_in_wanted = False
     for line in tbl:
+        print(line.strip().split()[1])
         if line.startswith(">Feature") and line.strip().split()[1] in wanted:
-            is_in_wanted = True
-            outfile.write(line)
-            continue
-        if is_in_wanted:
-            outfile.write(line)
-            continue
-        if line.startswith(">Feature") and line.strip().split()[1] not in wanted:
-            is_in_wanted = False
+            print("<MATCH>")
+        #     is_in_wanted = True
+        #     outfile.write(line)
+        #     continue
+        # if is_in_wanted:
+        #     outfile.write(line)
+        #     continue
+        # if line.startswith(">Feature") and line.strip().split()[1] not in wanted:
+        #     is_in_wanted = False
